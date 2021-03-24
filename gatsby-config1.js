67ba8b325c6be35fafecebceb77e447266568e1e
ConceptMiners <11329462+conceptminers1@users.noreply.github.com>
2021-03-25 00:28:35 +0530

    Update gatsby-config1.js

--- gatsby-config1.js
+++ gatsby-config1.js
@@ -1,31 +1,13 @@
 require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
-}) 
+})
 
 module.exports = {
-siteMetadata: {
-  siteUrl: `https://kalakrutplatformblog.gatsbyjs.io/`
-    },
   plugins: [
-    `gatsby-plugin-sitemap`,
-    `gatsby-plugin-robots-txt`,
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-sitemap`,
-      options: {
-      output: `/sitemap.xml`,
-      exclude: [`/settings`]
-      },
-    },
-    {
-      resolve: `gatsby-plugin-robots-txt`,
-      options: {
-      output: `/robots.txt`,
-      },
-    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -37,17 +19,17 @@ siteMetadata: {
       resolve: 'gatsby-source-cosmicjs',
       options: {
         bucketSlug: process.env.COSMIC_BUCKET,
-        objectTypes: ['posts','settings', 'services'],
+        objectTypes: ['posts','settings'],
         apiAccess: {
           read_key: process.env.COSMIC_READ_KEY,
         },
         localMedia: true
-      },
+      }
     },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: `UA-48640803-8`,
+        //trackingId: `UA-48640803-8`,
       },
     },
     {
